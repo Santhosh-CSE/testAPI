e7a0e629eda8498985236b01bbbafc07a60f5688
Santhosh-CSE <190030905cse@gmail.com>
2022-08-25 17:14:11 +0530

    first commit

--- app.js
+++ app.js
@@ -9,15 +9,15 @@ app.use(bodyParser.json());
 
 //Importing Routes
 const postsRoute = require('./routes/posts');
+const usersRoute = require('./routes/users');
 
 app.use('/posts', postsRoute);
+app.use('/users', usersRoute);
 
 //Routes
 app.get('/',(req,res)=>{
     res.send('We are on home');
 });
-
-
 
 //Connecting with DB
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true}, () => 
